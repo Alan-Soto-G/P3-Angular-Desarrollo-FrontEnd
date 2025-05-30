89b028d84489dfee98add54a3a01b9c43fbdfd0a
AlanSoto <ing.alansoto@gmail.com>
2025-05-30 08:06:43 -0500

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { AuthenticatedGuard } from './guardians/authenticated.guard';
+import { NoAuthenticatedGuard } from './guardians/no-authenticated.guard';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ComponentsModule,
+    NgbModule,
+    RouterModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent,
+    AuthLayoutComponent
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+    AuthenticatedGuard,
+    NoAuthenticatedGuard
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
